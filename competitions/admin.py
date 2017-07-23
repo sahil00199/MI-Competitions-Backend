7c833b610d6dd06a6671ee41221cb54b76928160
Sahil Shah <sahil00199@github.com>
2017-07-23 12:38:44 +0530

    changed the way indiiduals appear in admin page

--- competitions/admin.py
+++ competitions/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from .models import GroupEvent, IndividualEvent, Genre, Competition, Group
+from django import forms
 
 admin.site.register(GroupEvent)
-admin.site.register(IndividualEvent)
+#admin.site.register(IndividualEvent)
 admin.site.register(Genre)
 admin.site.register(Competition)
 admin.site.register(Group)
+
+class IndividualEventForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(IndividualEventForm, self).__init__(*args, **kwargs)
+        wtf = self.instance.participants.all()
+        w = self.fields['participants'].widget
+        choices = []
+        for choice in wtf:
+            choices.append((choice.id, choice.name))
+        w.choices = choices
+
+class IndividualEventAdmin(admin.ModelAdmin):
+    form = IndividualEventForm
+
+admin.site.register(IndividualEvent, IndividualEventAdmin)
