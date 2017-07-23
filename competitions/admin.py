456ca4ac036e797a8d586f87ba224741f927e50a
TheFlash98 <mastermind.sarthak@gmail.com>
2017-07-23 23:58:28 +0530

    Add events link on the group page

--- competitions/admin.py
+++ competitions/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import GroupEvent, IndividualEvent, Genre, Competition, Group
+from .forms import ImprovedModelForm
+
+
+
+class GroupAdmin(ImprovedModelForm):
+	raw_id_fields = ('event',"members",)
 
 admin.site.register(GroupEvent)
 admin.site.register(IndividualEvent)
 admin.site.register(Genre)
 admin.site.register(Competition)
-admin.site.register(Group)
+admin.site.register(Group,GroupAdmin)
+
